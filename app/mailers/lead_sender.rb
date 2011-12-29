0665608226159df82d9528c61d314500aee7e449
Amit Ambardekar <amitamb@gmail.com>
2011-12-30 00:58:19 +0530

    Added custom param

--- app/mailers/lead_sender.rb
+++ app/mailers/lead_sender.rb
@@ -3,6 +3,6 @@ class LeadSender < ActionMailer::Base
   
   def lead_mail(lead_mail)
     @lead_mail = lead_mail
-    mail(:to => "#{lead_mail.email} <#{lead_mail.email}>", :subject => "Test lead mail")
+    mail(:to => "#{lead_mail.email} <#{lead_mail.email}>", :subject => "Test lead mail", :"X-SMTPAPI" => {"unique_args":{"lead_mail_id":lead_mail.id}} )
   end
 end
--- app/models/lead_mail.rb
+++ app/models/lead_mail.rb
@@ -4,18 +4,18 @@
 # The actual mail is send using a mailer that is not included in this project.
 #
 class LeadMail < ActiveRecord::Base
-  
+
   # This is how it works in reality
   # belongs_to :lead, :counter_cache => true
   # belongs_to :mail_template
   # belongs_to :from_user, :class_name => "User", :foreign_key => "from_user_id", :counter_cache => :send_lead_mails_count
-  
+
   def send_this_week?
     created_at.year == Date.today.year && created_at.strftime("%U") == Date.today.strftime("%U")
   end
-  
+
   after_create :send_email
-  
+
   #private
   
   def send_email
