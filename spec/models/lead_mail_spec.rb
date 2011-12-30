c3fbf4648782409498ce7bf7faa213b0f2ccedc5
Amit Ambardekar <amitamb@gmail.com>
2011-12-31 01:05:37 +0530

    Some updates to test

--- spec/models/lead_mail_spec.rb
+++ spec/models/lead_mail_spec.rb
@@ -13,10 +13,10 @@ describe LeadMail do
       new_lm = LeadMail.create( :email => "test@example.com" )
       new_lm.should_not be_nil
       mail = ActionMailer::Base.deliveries.last
-      assert_equal 'amitamb@gmail.com', mail['from'].to_s
-      assert_equal 'test@example.com <test@example.com>', mail['to'].to_s
-      assert_equal 'Test lead mail', mail['Subject'].to_s
-      assert_equal "\nThis is a test mail sent to #{ new_lm.email }\n\nThis is a test link <a href=\"http://www.verticalset.com/\">VerticalSet</a>.\n", mail.body.to_s
+      mail['from'].to_s.should == 'amitamb@gmail.com'
+      mail['to'].to_s.should == 'test@example.com <test@example.com>'
+      mail['Subject'].to_s.should == 'Test lead mail'
+      mail.body.to_s.should == "\nThis is a test mail sent to #{ new_lm.email }\n\nThis is a test link <a href=\"http://www.verticalset.com/\">VerticalSet</a>.\n"
     end
   end
 end
